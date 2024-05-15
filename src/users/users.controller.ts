95732724832faf7656305ed062160f2fa8c73b7e
Vlad Chmara <chmaravlad5@gmail.com>
2024-05-15 22:23:04 +0300

    updated/ info controller users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get, Request, Res, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { AccessJwtAuthGuard } from 'src/auth/access-token-jwt-auth.guard';
 import { AuthService } from 'src/auth/auth.service';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(AccessJwtAuthGuard)
   @Get('/info')
   async user(@Request() req, @Res({ passthrough: true }) res: Response) {
     // Passport automatically creates a user object, based on the value we return from the validate() method,
     // and assigns it to the Request object as req.user. Later, we'll replace this with code to create and return a JWT instead
 
-    return { msg: 'success' };
+    return { user: req.user };
   }
 }
