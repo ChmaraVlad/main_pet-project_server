798f7b43cad715e83c3a3bf61598746f09eeb7bf
Vlad Chmara <chmaravlad5@gmail.com>
2024-05-15 16:38:57 +0300

    installed swagger docs and updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Advanced lesson Backend')
+    .setDescription('Documentation REST API')
+    .setVersion('1.0.0')
+    .addTag('main')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.use(cookieParser());
   app.enableCors();
-  await app.listen(5000);
+  //we can add global pipes for all routes, here can be lots of pipes, one by one
+  // app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
