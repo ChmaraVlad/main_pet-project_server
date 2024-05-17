c7423a0227b2fcaae08af622d18fe3cff9e09dd0
Vlad Chmara <chmaravlad5@gmail.com>
2024-05-17 17:48:25 +0300

    updated user services and controller, added isAdmin checking

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [UsersService],
+  providers: [UsersService, ConfigModule],
   exports: [UsersService],
   controllers: [UserController],
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), ConfigModule],
 })
 export class UsersModule {}
